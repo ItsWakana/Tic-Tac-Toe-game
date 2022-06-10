70868be8915bc3b9b6dfe2c503db924ed7049732
MultiFlex <wakanawardrobe@gmail.com>
2022-06-10 15:48:34 +0100

    add basic gameboard to html

---
+++ script.js
@@ -0,0 +1,14 @@
+const myModule = (function () {
+
+    const createGamePlayer = (name, id) => {
+        return { name, id };
+    }
+    
+    const james = createGamePlayer('James', 1);
+
+})();
+
+
+
+
+
