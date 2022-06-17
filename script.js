208be69cbf620471802f612ec1ab086fafe48693
MultiFlex <wakanawardrobe@gmail.com>
2022-06-17 12:31:55 +0100

    add transition effects for answers

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const myModule = (function () {
 
     const mainBoard = document.querySelector('[data-game-board]');
     const boxes = [...mainBoard.querySelectorAll('.box')];
+    const innerAnswerText = [...document.querySelectorAll('p')];
     const row1 = [boxes[0],boxes[1],boxes[2]];
     const row2 = [boxes[3],boxes[4],boxes[5]];
     const row3 = [boxes[6],boxes[7],boxes[8]];
@@ -19,22 +20,21 @@ const myModule = (function () {
 
     const gameLogic = (() => {
 
-        boxes.forEach(box => {
+        boxes.forEach((box, index) => {
 
             box.dataset.taken = 'false';
             box.addEventListener('click', () => {
                 playerMove(box);
-                const playerWinCheck = ryan.checkWin(box);
-                console.log(playerWinCheck)
-
-                if (playerWinCheck !== undefined) {
-                    gameBoardArray = [0,1,2,3,4,5,6,7,8];
+                if (ryan.checkWin() !== undefined) {
                     clearTheBoard();
                     return;
                 }
 
                 computerMove();
-                computer.checkWin();
+                if (computer.checkWin() !== undefined) {
+                    clearTheBoard();
+                    return;
+                }
             });
         });
 
@@ -46,10 +46,11 @@ const myModule = (function () {
         }
 
         const clearTheBoard = () => {
-            boxes.forEach(box => {
+            gameBoardArray = [0,1,2,3,4,5,6,7,8];
+            boxes.forEach((box, index) => {
                 box.dataset.taken = 'false';
                 box.dataset.result = '';
-                box.innerText = '';
+                innerAnswerText[index].classList.remove('show');
             });
         }
 
@@ -62,7 +63,7 @@ const myModule = (function () {
 
     const Player = (name, team) => {
 
-        const checkWin = (box) => {
+        const checkWin = () => {
             const equalsX = (box) => box.dataset.result == team;
             if (row1.every(equalsX) == true || row2.every(equalsX) == true ||
                 row3.every(equalsX) == true || col1.every(equalsX) == true ||
@@ -70,15 +71,14 @@ const myModule = (function () {
                 diag1.every(equalsX) == true || diag2.every(equalsX) == true) {
                 alert(name + ' wins!');
                 return name;
-                
             }
-
         }
 
         let answerOnDisplay = (index) => {
+            innerAnswerText[index].classList.add('show');
             gameBoardArray.splice(index, 1, team);
             boxes[index].dataset.result = team;
-            boxes[index].innerText = gameBoardArray[index];
+            innerAnswerText[index].innerText = gameBoardArray[index];
             boxes[index].dataset.taken = 'true';
         }
 
