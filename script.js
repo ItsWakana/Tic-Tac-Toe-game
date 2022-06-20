84e2089b3863fd5bf217abf50e2d0934e7de3ec9
MultiFlex <wakanawardrobe@gmail.com>
2022-06-20 15:52:11 +0100

    add prompt for user to enter their name

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 
 
-const gameBoardModule = (function () {
+const gameBoardModule = (() => {
 
     const mainBoard = document.querySelector('[data-game-board]');
     const boxes = [...mainBoard.querySelectorAll('.box')];
-    const button = document.querySelector('button');
+    const button = document.querySelector('.start-game');
+    const modalSubmit = document.querySelector('.submit');
     const innerAnswerText = [...document.querySelectorAll('p')];
-    const row1 = [boxes[0],boxes[1],boxes[2]];
-    const row2 = [boxes[3],boxes[4],boxes[5]];
-    const row3 = [boxes[6],boxes[7],boxes[8]];
 
-    const col1 = [boxes[0],boxes[3],boxes[6]];
-    const col2 = [boxes[1],boxes[4],boxes[7]];
-    const col3 = [boxes[2],boxes[5],boxes[8]];
+    const gameLogic = (() => {
 
-    const diag1 = [boxes[0],boxes[4],boxes[8]];
-    const diag2 = [boxes[2],boxes[4],boxes[6]];
+        const row1 = [boxes[0],boxes[1],boxes[2]];
+        const row2 = [boxes[3],boxes[4],boxes[5]];
+        const row3 = [boxes[6],boxes[7],boxes[8]];
+    
+        const col1 = [boxes[0],boxes[3],boxes[6]];
+        const col2 = [boxes[1],boxes[4],boxes[7]];
+        const col3 = [boxes[2],boxes[5],boxes[8]];
+    
+        const diag1 = [boxes[0],boxes[4],boxes[8]];
+        const diag2 = [boxes[2],boxes[4],boxes[6]];
+    
+        let gameBoardArray = [0,1,2,3,4,5,6,7,8];
 
-    let gameBoardArray = [0,1,2,3,4,5,6,7,8];
-
-    const game = () => {
-
-        boxes.forEach((box) => {
-            box.dataset.taken = 'false';
-            box.onclick = () => {
-                makeMoves(box);
-            };
-        });
+        let myPlayers = [];
 
         const clearTheBoard = () => {
             mainBoard.classList.remove('no-pointer');
@@ -40,45 +37,110 @@ const gameBoardModule = (function () {
 
         const makeMoves = (box) => {
 
-            ryan.playerMove(box);
-            if (ryan.checkWin() !== undefined) {
-                ryan.displayGameResult();
+            let move = gameLogic.myPlayers[0].playerMove(box);
+            if (gameLogic.myPlayers[0].checkWin(gameLogic) !== undefined) {
+                gameLogic.myPlayers[0].displayGameResult();
                 mainBoard.classList.add('no-pointer');
                 return;
             }
-
-            // setTimeout(computerMove, 600);
-            computer.computerMove();
-            if (computer.checkWin() !== undefined) {
-                computer.displayGameResult();
-                mainBoard.classList.add('no-pointer');
-                return;
+            if (move == 'yes') {
+            } else {
+                // setTimeout(computerMove, 600);
+                computer.computerMove(box);
+                if (computer.checkWin(gameLogic) !== undefined) {
+                    computer.displayGameResult();
+                    mainBoard.classList.add('no-pointer');
+                    return;
+                }
             }
         }
 
-        return { clearTheBoard };
-    }
+        const openModal = (modal,overlay) => {
+            if (modal == null) return;
+            modal.classList.add('active');
+            overlay.classList.add('active');
+        }
+
+        const closeModal = (modal,overlay) => {
+            if (modal == null) return;
+            modal.classList.remove('active');
+            overlay.classList.remove('active');
+        }
+
+        const addNewPlayer = (e) => {
+            e.preventDefault();
+
+            let playerName = document.getElementById('name').value
+
+            const newPlayer = Player(playerName, 'X');
+            return newPlayer;
+        }
+
+        const gameRound = (newPlayer) => {
+            gameLogic.myPlayers.splice(0, 1 ,newPlayer);
+            console.log(gameLogic.myPlayers[0].name);
+            gameLogic.closeModal(modal,overlay);
+            const resultsTable = document.querySelector('.results-table');
+            resultsTable.classList.remove('show');
+            playGame.gameStart();
+            gameLogic.clearTheBoard();
+        }
+
+        return { clearTheBoard, makeMoves, openModal, closeModal, addNewPlayer, gameRound,
+                row1,row2,row3,col1,col2,col3,diag1,diag2,gameBoardArray, myPlayers };
+    })();
+
+    const playGame = (() => {
+
+        const modal = document.querySelector('.user-input');
+        const overlay = document.querySelector('#overlay');
+
+        const gameStart = () => {
+            boxes.forEach((box) => {
+                box.dataset.taken = 'false';
+                box.onclick = () => {
+                    gameLogic.makeMoves(box);
+                };
+            });
+        }
+
+        button.addEventListener('click', () => {
+            gameLogic.openModal(modal, overlay);
+        });
+    
+        overlay.addEventListener('click', () => {
+            gameLogic.closeModal(modal,overlay);
+        });
+    
+        modalSubmit.addEventListener('click', (e) => {
+            let newPlayer = gameLogic.addNewPlayer(e);
+            gameLogic.gameRound(newPlayer);
+        });
+
+        return { gameStart }
+
+})();
 
     const Player = (name, team) => {
 
         const playerMove = (box) => {
             if (box.dataset.taken == 'true') {
-                return;
+                return 'yes';
             }
-            ryan.answerOnDisplay(box.dataset.id);
+            gameLogic.myPlayers[0].answerOnDisplay(box.dataset.id);
         }
 
-        const computerMove = () => {
+        const computerMove = (box) => {
             const computerIndex = computer.computerAnswer();
             computer.answerOnDisplay(computerIndex);
         }
 
-        const checkWin = () => {
-            const equalsX = (box) => box.dataset.result == team;
-            if (row1.every(equalsX) == true || row2.every(equalsX) == true ||
-                row3.every(equalsX) == true || col1.every(equalsX) == true ||
-                col2.every(equalsX) == true || col3.every(equalsX) == true ||
-                diag1.every(equalsX) == true || diag2.every(equalsX) == true) {
+        const checkWin = (game) => {
+            const equalsXorO = (box) => box.dataset.result == team;
+            if (game.row1.every(equalsXorO) == true || game.row2.every(equalsXorO) == true ||
+                game.row3.every(equalsXorO) == true || game.col1.every(equalsXorO) == true ||
+                game.col2.every(equalsXorO) == true || game.col3.every(equalsXorO) == true ||
+                game.diag1.every(equalsXorO) == true || game.diag2.every(equalsXorO) == true) {
                 
                 return name;
             }
@@ -96,9 +158,9 @@ const gameBoardModule = (function () {
 
         let answerOnDisplay = (index) => {
             innerAnswerText[index].classList.add('show');
-            gameBoardArray.splice(index, 1, team);
+            gameLogic.gameBoardArray.splice(index, 1, team);
             boxes[index].dataset.result = team;
-            innerAnswerText[index].innerText = gameBoardArray[index];
+            innerAnswerText[index].innerText = gameLogic.gameBoardArray[index];
             boxes[index].dataset.taken = 'true';
         }
 
@@ -119,14 +181,7 @@ const gameBoardModule = (function () {
 
         return { name, team, answerOnDisplay, checkWin, computerAnswer, displayGameResult, playerMove, computerMove };
     }
-    
-    const ryan = Player('Ryan', 'X');
-    const computer = Player('Computer', 'O');
 
-    button.addEventListener('click', () => {
-        const resultsTable = document.querySelector('.results-table');
-        resultsTable.classList.remove('show');
-        game().clearTheBoard();
-    });
+    const computer = Player('Computer', 'O');
 
 })();
