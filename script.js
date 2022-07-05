efb56797bb61adfcc2ccaa2385cdb078cd217f5b
MultiFlex <wakanawardrobe@gmail.com>
2022-07-05 15:57:22 +0100

    add gameboard module

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const gameBoardModule = (() => {
+    const mainBoard = document.querySelector('[data-game-board]');
+    const boxes = [...mainBoard.querySelectorAll('.box')];
+    const button = document.querySelector('.start-game');
+    const innerAnswerText = [...document.querySelectorAll('p')];
